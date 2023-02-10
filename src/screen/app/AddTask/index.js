36b7f0edcb83e3472c97f617748b2728394438f1
Rufino Rojas <ruf_rm@hotmail.com>
2023-02-09 20:02:22 -0600

    add task in collection firestore

--- src/screen/app/AddTask/index.js
+++ src/screen/app/AddTask/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, Image, SafeAreaView, Pressable, Alert } from 'react-native';
+import { View, Text, Image, SafeAreaView, Pressable, Alert, ActivityIndicator, ScrollView } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
 import Button from '../../../components/button';
 import styles from './styles';
 import Title from '../../../components/title';
@@ -13,7 +15,7 @@ const AddTask = ({ navigation }) => {
     const [title, setTitle] = useState('')
     const [category, setCategory] = useState();
     const [deadline, setDeadline] = useState(new Date());
-
+    const [loading, setLoading] = useState(false);
 
     const handleBack = () => {
         navigation.goBack();
@@ -32,27 +34,54 @@ const AddTask = ({ navigation }) => {
             Alert.alert('Please enter future date');
             return;
         }
+
+        setLoading(true);
+        firestore()
+            .collection('Task')
+            .doc('ABC')
+            .set({
+                title,
+                deadline,
+                category
+            })
+            .then(() => {
+                setLoading(false);
+                navigation.navigate('Tasks');
+                setTitle('');
+                setDeadline(new Date());
+                setCategory(null);
+            }).catch(e => {
+                console.log('ERROR =====>', e);
+                setLoading(false);
+                Alert.alert(e.message);
+            });
     }
 
     return (
         <>
             <SafeAreaView style={styles.container}>
-                <Pressable style={styles.backContainer} hitSlop={8} onPress={handleBack}>
-                    <Image style={styles.backIcon} source={require('../../../assets/back.png')} />
-                </Pressable>
+                <ScrollView>
+                    <Pressable style={styles.backContainer} hitSlop={8} onPress={handleBack}>
+                        <Image style={styles.backIcon} source={require('../../../assets/back.png')} />
+                    </Pressable>
 
-                <Title type="thin">Add New Task</Title>
+                    <Title type="thin">Add New Task</Title>
 
-                <Text style={styles.label}>Describe the task</Text>
-                <Input value={title} onChangeText={setTitle} outlined placeholder="Type here..." />
+                    <Text style={styles.label}>Describe the task</Text>
+                    <Input value={title} onChangeText={setTitle} outlined placeholder="Type here..." />
 
-                <Text style={styles.label}>Type</Text>
-                <Categories categories={categories} selectedCategory={category} onCategoryPress={setCategory} />
+                    <Text style={styles.label}>Type</Text>
+                    <Categories categories={categories} selectedCategory={category} onCategoryPress={setCategory} />
 
-                <Text style={styles.label}>Deadline</Text>
-                <DateInput value={deadline} onChange={setDeadline} />
+                    <Text style={styles.label}>Deadline</Text>
+                    <DateInput value={deadline} onChange={setDeadline} />
 
-                <Button style={styles.button} type="blue" onPress={onSubmit}>Add the Task</Button>
+                    {
+                        loading ? <ActivityIndicator /> : (
+                            <Button style={styles.button} type="blue" onPress={onSubmit}>Add the Task</Button>
+                        )
+                    }
+                </ScrollView>
             </SafeAreaView>
         </>
     )
